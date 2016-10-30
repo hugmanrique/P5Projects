ff6dd9fb3c4110d31a87031de093f6b0ffac1adb
hugmanrique <hugmanrique@gmail.com>
2016-10-30 17:01:39 -0400

    Removed CORS shit

--- projects/circlelogo/main.js
+++ projects/circlelogo/main.js
@@ -28,8 +28,6 @@ var numSlider;
 var created = false;
 
 function preload() {
-    // p5.js CORS hack
-    overwrite();
     var queried = getQueryUrl();
     
     image = loadImage(queried || "projects/circlelogo/paisaje.jpg");
@@ -172,37 +170,3 @@ Circle.prototype.getColor = function () {
         this.color[3] = minAlpha;
     }
 };
-
-// P5.js loadImage CORS hack
-function overwrite(){
-    p5.prototype.loadImage = function(path, successCallback, failureCallback) {
-        var img = new Image();
-        var pImg = new p5.Image(1, 1, this);
-        var decrementPreload = p5._getDecrementPreload.apply(this, arguments);
-
-        img.onload = function() {
-            pImg.width = pImg.canvas.width = img.width;
-            pImg.height = pImg.canvas.height = img.height;
-            pImg.drawingContext.drawImage(img, 0, 0);
-
-            if (typeof successCallback === 'function') {
-                successCallback(pImg);
-            }
-            if (decrementPreload && (successCallback !== decrementPreload)) {
-                decrementPreload();
-            }
-        };
-        img.onerror = function(e) {
-            p5._friendlyFileLoadError(0,img.src);
-            if ((typeof failureCallback === 'function') &&
-                (failureCallback !== decrementPreload)) {
-                failureCallback(e);
-            }
-        };
-
-        img.crossOrigin = 'Anonymous';
-        img.src = path;
-
-        return pImg;
-    };
-}
