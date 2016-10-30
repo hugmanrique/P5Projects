f9bb1f21fbb4a95eee1901572469d4deebd0791f
hugmanrique <hugmanrique@gmail.com>
2016-10-30 16:27:32 -0400

    Ended ImgCircle

---
+++ projects/circlelogo/main.js
@@ -0,0 +1,172 @@
+/*
+Created by Hugmanrique. Feel free to modify giving credit
+
+Challenges:
+- Instead of modify alpha colors, use the nearest real color when finding for each circle
+- Use a noise function instead of random when choosing which circle to split.
+  You can even use algorithms which use a particle modified by forces.
+
+Good luck!
+ */
+
+var circles = [];
+var image;
+
+const baseSize = 64;
+const fps = 10;
+const minAlpha = 20;
+
+var splitPerFps;
+var divide;
+var maxDepth = 3;
+
+const bg = 51;
+
+var splitSlider;
+var numSlider;
+
+var created = false;
+
+function preload() {
+    var queried = getQueryUrl();
+    
+    image = loadImage(queried || "projects/circlelogo/paisaje.jpg");
+}
+
+function getQueryUrl() {
+    var qs = document.location.search;
+    qs = qs.split('+').join(' ');
+
+    var params = {},
+        tokens,
+        re = /[?&]?([^=]+)=([^&]*)/g;
+
+    while (tokens = re.exec(qs)) {
+        params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+    }
+
+    return params['url'];
+}
+
+function setup(){
+    var width = image.width; //+ (image.width % baseSize);
+    var height = image.height; //+ (image.height % baseSize);
+    
+    createCanvas(width, height);
+    
+    splitSlider = createSlider(1, 20, 5);
+    numSlider = createSlider(2, 4, 2);
+    
+    frameRate(fps);
+    pixelDensity(1);
+    
+    background(bg);
+    ellipseMode(CORNER);
+}
+
+function draw(){
+    splitPerFps = splitSlider.value();
+    divide = numSlider.value();
+    
+    if (!created && circles.length === 0) {
+        // Create initial circles
+        createCircles();
+        created = true;
+    }
+    
+    // Get circle to split
+    for (var split = 0; split < splitPerFps; split++) {
+        if (splitCircle()) {
+            // We are done!
+            noLoop();
+            console.log('Done!');
+            break;
+        }
+    }
+}
+
+function splitCircle() {
+    if (circles.length === 0) {
+        return true;
+    }
+    
+    var toSplit = random(circles);
+
+    if (toSplit.depth <= maxDepth) {
+        var lastLayer = toSplit.depth == maxDepth - 1;
+        toSplit.split(!lastLayer);
+        
+        return false;
+    }
+}
+
+// Circle related stuff
+function createCircles() {
+    var maxHeight = round(height / baseSize);
+    var maxWidth = round(width / baseSize);
+    
+    for (var y = 0; y < maxHeight; y++) {
+        for (var x = 0; x < maxWidth; x++) {
+            var circle = new Circle(x * baseSize, y * baseSize, baseSize);
+            circle.draw();
+            
+            circles.push(circle);
+        }
+    }
+}
+
+var Circle = function (x, y, size, depth) {
+    this.pos = createVector(x, y);
+    this.depth = depth || 0;
+    this.size = size;
+    
+    this.getColor();
+};
+
+Circle.prototype.draw = function () {
+    fill(this.color);
+    noStroke();
+    
+    ellipse(this.pos.x, this.pos.y, this.size, this.size);
+};
+
+Circle.prototype.undraw = function () {
+    fill(bg);
+    noStroke();
+
+    rect(this.pos.x, this.pos.y, this.size, this.size);
+};
+
+Circle.prototype.split = function (addToArray) {
+    var subSize = this.size / divide;
+    var depth = this.depth + 1;
+    
+    var index = circles.indexOf(this);
+    circles.splice(index, 1);
+    
+    // Remove ellipse from this circle
+    this.undraw();
+    
+    for (var i = 0; i < divide; i++) {
+        for (var j = 0; j < divide; j++) {
+            var xCircle = this.pos.x + (subSize * j);
+            var yCircle = this.pos.y + (subSize * i);
+            
+            var newCircle = new Circle(xCircle, yCircle, subSize, depth);
+            newCircle.draw();
+
+            if (addToArray) {
+                circles.push(newCircle);
+            }
+        }
+    }
+};
+
+Circle.prototype.getColor = function () {
+    this.color = image.get(this.pos.x, this.pos.y);
+    
+    // We don't want alpha
+    if (this.color[3] < minAlpha){
+        this.color[3] = minAlpha;
+    }
+};
